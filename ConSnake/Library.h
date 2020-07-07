e2c354120cd960319461e9acf2656345939e9be0
DangHaiThinh <67593021+DangHaiThinh@users.noreply.github.com>
2020-07-07 23:26:18 +0700

    Update Library.h

--- ConSnake/Library.h
+++ ConSnake/Library.h
@@ -1,4 +1,6 @@
 #pragma once
+  
+#pragma once
 
 #include <iostream>
 #include <time.h>
@@ -9,7 +11,29 @@
 #include <fstream>
 
 using namespace std;
-enum class Symbol { LONG_BIG_BLOCK = 219, SQUARE_BLOCK = 254, LONG_THIN_LEFT_BLOCK = 222, LONG_THIN_RIGHT_BLOCK = 221, LONG_TOP_BLOCK = 220, LONG_BOTTOM_BLOCK = 223, APPLE = 153 };
+
+//struct
+struct Board
+{
+	char Name[9];
+	unsigned int Score;
+};
+struct Coordinate
+{
+	int x, y;
+};
+
+//ascii code
+enum class Symbol
+{
+	LONG_BIG_BLOCK = 219,
+	SQUARE_BLOCK = 254,
+	LONG_THIN_LEFT_BLOCK = 222,
+	LONG_THIN_RIGHT_BLOCK = 221,
+	LONG_TOP_BLOCK = 220,
+	LONG_BOTTOM_BLOCK = 223,
+	APPLE = 153
+};
 
 //constant
 const short int MAX_B = 11;
@@ -21,17 +45,43 @@ const short int INIT_SNAKE_LENGTH = 4;
 const short int INIT_FOOD_COUNTER = 0;
 const short int FOOD = 1;
 
-//bien mau
+//su dung cho cac windows func
 static HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-enum class Color {RED = 0, GREEN = 1, BLUE = 2, YELLOW = 3, PURPLE = 4};
+
+//bien mau
+enum class Color
+{
+	RED = 0,
+	GREEN = 1,
+	BLUE = 2,
+	YELLOW = 3,
+	PURPLE = 4
+};
 
 //game mode 
-enum class GameMode { EASY = 0, NORMAL = 1, HARD = 2, SPECIAL = 3 };
+enum class GameMode
+{
+	EASY = 0,
+	NORMAL = 1,
+	HARD = 2,
+	SPECIAL = 3
+};
 
 //thua neu gia tri am
-enum class MapData {WALL = -2, SNAKE = -1, NOTHING = 0};
+enum class MapData
+{
+	WALL = -2,
+	SNAKE = -1,
+	NOTHING = 0
+};
 
-enum class SnakeDirection { RIGHT = 0, UP = 1, LEFT = 2, DOWN = 3, EXIT = -1 };
+enum class SnakeDirection {
+	RIGHT = 0,
+	UP = 1,
+	LEFT = 2,
+	DOWN = 3,
+	EXIT = -1
+};
 
 //huong di
 static short int dx[5] = { 1, 0, -1, 0 };
@@ -42,11 +92,11 @@ static unsigned short int speed[5]{ 0, 34 , 74 , 0 };
 //(0, -1) = len
 //(-1, 0) = trai
 //(0, 1) = xuong
-short int prevInput = LEFT;
-short int input = LEFT;	//bien toan cuc de lay input
-short int item = NOTHING;
+SnakeDirection prevInput = SnakeDirection::LEFT;
+SnakeDirection input = SnakeDirection::LEFT;	//bien toan cuc de lay huong di chuyen cua ran
+MapData item = MapData::NOTHING;
 
-void gotoxy(int column, int row)
+static void gotoXY(int column, int row)
 {
 	HANDLE hStdOut;
 	COORD coord;
@@ -59,7 +109,7 @@ void gotoxy(int column, int row)
 	SetConsoleCursorPosition(hStdOut, coord);
 }
 
-void clearScreen()
+static void clearScreen()
 {
 	HANDLE                     hStdOut;
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
@@ -70,68 +120,52 @@ void clearScreen()
 	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	if (hStdOut == INVALID_HANDLE_VALUE) return;
 
-	/* Get the number of cells and cell attributes in the current buffer */
 	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
 	cellCount = csbi.dwSize.X * csbi.dwSize.Y;
 
-	/* Fill the entire buffer with spaces */
 	if (!FillConsoleOutputCharacter(
-		hStdOut,		//handle to console screen buffer
-		(TCHAR)' ',	//character to write to the buffer
-		cellCount,		//number of cells to write to
-		homeCoords,		//coordinates of first cell
-		&count			//receives the number of characters written
+		hStdOut,		
+		(TCHAR)' ',	
+		cellCount,		
+		homeCoords,		
+		&count			
 	)) return;
-
-	/* Fill the entire buffer with the current colors and attributes */
 	if (!FillConsoleOutputAttribute(
-		hStdOut,			//handle to console screen buffer
-		csbi.wAttributes,	//Character attributes to use
-		cellCount,			//Number of cells to set attribute
-		homeCoords,			//Coordinate of first cell
-		&count				//receives the number of characters written
+		hStdOut,			
+		csbi.wAttributes,	
+		cellCount,			
+		homeCoords,			
+		&count				
 	)) return;
-
-	/* Move the cursor home */
 	SetConsoleCursorPosition(hStdOut, homeCoords);
 }
 
-void setColor(Color x)
+static void setColor(Color _color)
 {
-	switch (x)
+	switch (_color)
 	{
-	case Color::RED :
+	case Color::RED:
 		SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_INTENSITY);
 		break;
-	case Color::GREEN :
-		SetConsoleTextAttribute(h, FOREGROUND_GREEN| FOREGROUND_INTENSITY);
+	case Color::GREEN:
+		SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 		break;
-	case Color::BLUE :
+	case Color::BLUE:
 		SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
 		break;
-	case Color::YELLOW :
+	case Color::YELLOW:
 		SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN);
 		break;
-	case Color::PURPLE :
+	case Color::PURPLE:
 		SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_BLUE);
 		break;
 	}
 }
 
-void hideCursor()
+static void hideCursor()
 {
-	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);  
-	CONSOLE_CURSOR_INFO info;  
-	info.dwSize = 100;   info.bVisible = false;  
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 100;   info.bVisible = false;
 	SetConsoleCursorInfo(consoleHandle, &info);
 }
-
-struct Board
-{
-	char Name[9];
-	unsigned int Score;
-};
-struct Coordinate
-{
-	int x, y;
-};
